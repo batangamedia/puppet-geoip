1190ff9f6ab2f6e1f97ff432d7180a575c16647d
Martin Loy <martinloy.uy@gmail.com>
2014-12-08 11:59:16 -0200

    dont install if no credentials provided

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,11 +6,16 @@ class geoip inherits geoip::params {
  
   package { $geoip::params::package: ensure => installed }
 
-  class {'geoip::conf':
-    require => Package[$geoip::params::package]
-  } -> exec {'run geoipupdate':
-        command => '/usr/bin/geoipupdate',
-        unless  => '/usr/bin/test -f /usr/share/GeoIP/GeoIPCity.dat'
+
+  if ($geoip::params::userid and $geoip::params::licensekey) {
+    class {'geoip::conf':
+      require => Package[$geoip::params::package]
+    } -> exec {'run geoipupdate':
+      command => '/usr/bin/geoipupdate',
+      unless  => '/usr/bin/test -f /usr/share/GeoIP/GeoIPCity.dat'
+    }
+  } else {
+    notify{ 'Geoip module not configured, credentials missings':  }
   }
 
   if ($geoip::params::update_cron) {
