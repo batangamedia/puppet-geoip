7c8b9ba9a574413c7fdf17eac1087d6cbeb684a1
Martin Loy <martinloy.uy@gmail.com>
2015-03-23 09:20:15 -0300

    debuging cron

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,10 +3,9 @@
 # GeoIP configuration used by other classes.
 #
 class geoip inherits geoip::params {
- 
+
   package { $geoip::params::package: ensure => installed }
-
-
+  
   if ($geoip::params::userid and $geoip::params::licensekey) {
     class {'geoip::conf':
       require => Package[$geoip::params::package]
@@ -14,17 +13,20 @@ class geoip inherits geoip::params {
       command => '/usr/bin/geoipupdate',
       unless  => '/usr/bin/test -f /usr/share/GeoIP/GeoIPCity.dat'
     }
-  } else {
-    notify{ 'Geoip module not configured, credentials missings':  }
-  }
 
-  if ($geoip::params::update_cron) {
+    if ($geoip::params::update_cron) {
 
-    file{'/etc/cron.weekly/geoip-update':
-        ensure => present,
-        source => 'puppet:///modules/geoip/geoipupdate.cron'
+      cron { 'geoipupdate':
+        ensure  => present,
+        command => '/usr/bin/geoipupdate',
+        minute  => '*/5',
+        user    => root
+      }
+
     }
 
+  } else {
+    notify{ 'Geoip module not configured, credentials missings':  }
   }
 
 }
