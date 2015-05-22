b40442e3fa3da34b3fc44ef25db07770b578697e
Guzman Braso <guzman@guruhub.com.uy>
2015-05-22 16:11:23 -0300

    geoip-database not optional anymore

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,6 +4,6 @@ class geoip::params {
   $userid        = hiera('geoip::userid', '')
   $licensekey    = hiera('geoip::licensekey', '')
   $productids    = hiera('geoip::products', '106 132')
-  $extra_package = hiera('geoip::extra_package', false)
+  $extra_package = hiera('geoip::extra_package', 'geoip-database')
   $update_cron   = hiera('geoip::update_cron', true)
 }
