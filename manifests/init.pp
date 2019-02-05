9ab21c0bc5fbda91d23fac3afbbea92dced6d217
Guzmán Brasó <guzman.braso@gmail.com>
2019-02-05 17:48:58 -0300

    Removed syntax not compatible with puppet3 and freeze it inside branch 'puppet3-compatible'

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,15 +10,8 @@ class geoip inherits geoip::params {
   	package { $geoip::params::extra_package: ensure => installed }  
   }
   
-  if ( $facts['os']['family'] == 'Debian' and $facts['os']['distro']['release']['major'] == '9' ) {
-    package{'geoipupdate':
-      ensure => installed
-    }
-    # Debian 9 changes default path to /var/lib/GeoIP
-    $real_save_path = '/var/lib/GeoIP'
-  } else {
-    $real_save_path = $geoip::params::save_path
-  }
+  $real_save_path = $geoip::params::save_path
+
   if ($geoip::params::userid and $geoip::params::licensekey) {
     class {'geoip::conf':
       require => Package[$geoip::params::package]
