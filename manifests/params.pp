2bcf87c4d90c0b773efc0a4d62b4d3f981868772
Guzmán Brasó <guzman.braso@gmail.com>
2019-02-12 13:47:10 -0300

    Fixed level 8 mix

--- manifests/params.pp
+++ manifests/params.pp
@@ -6,5 +6,5 @@ class geoip::params {
   $productids    = hiera('geoip::products', '106 132')
   $extra_package = hiera('geoip::extra_package', 'geoip-database')
   $update_cron   = hiera('geoip::update_cron', true)
-  $save_path     = hiera('groip::save_path', '/usr/share/logster')
+  $save_path     = hiera('groip::save_path', '/usr/share/GeoIP')
 }
